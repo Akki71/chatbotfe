2696f8a029d6439efc892dd21a8f6fe1b17ddb76
AquilMedia <accounts@aquilmedia.in>
2026-01-27 18:25:15 +0530

    changes for gulf pharmacy bot

--- public/seo-chatbot.js
+++ public/seo-chatbot.js
@@ -14,7 +14,7 @@
     currentScript?.dataset.primaryColor || "#6366f1";
 
   const CHATBOT_URL =
-    `http://r480gw4ogwocoo44g0og08k8.217.217.248.153.sslip.io/chatbot` +
+    `https://chatbot.preproductiondemo.com/chatbot` +
     `?projectName=${encodeURIComponent(PROJECT_NAME)}` +
     `&projectId=${encodeURIComponent(PROJECT_ID)}` +
     `&color=${encodeURIComponent(PRIMARY_COLOR)}`;
